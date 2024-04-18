a78a26e43a41ec515f9a507e967b06ce322f04f3
NaimoonJannat <naimoonjannat@gmail.com>
2024-04-18 19:03:34 +0600

    Header Added

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,9 +1,11 @@
 
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 const Root = () => {
     return (
-        <div>
+        <div className='w-4/5 mx-auto'>
+            <Header></Header>
             <Outlet></Outlet>
         </div>
     );
